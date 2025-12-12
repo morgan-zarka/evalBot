45b73702ea6b91ac3399c02dd304ae7ad9f6578f
FARAH Mohamed <mohamed.farah@edu.esiee.fr>
2025-12-12 09:24:02 +0100

    Configuration LEDS et SWITCHERS

---
+++ Leds.s
@@ -0,0 +1,106 @@
+AREA    |.text|, CODE, READONLY
+		ALIGN
+
+; This register controls the clock gating logic in normal Run mode
+SYSCTL_PERIPH_GPIO EQU		0x400FE108	; SYSCTL_RCGC2_R (p291 datasheet de lm3s9b92.pdf)
+
+; The GPIODATA register is the data register
+GPIO_PORTF_BASE		EQU		0x40025000	; GPIO Port F (APB) base: 0x4002.5000 (p416 datasheet de lm3s9B92.pdf)
+
+; The GPIODATA register is the data register
+GPIO_PORTD_BASE		EQU		0x40007000		; GPIO Port D (APB) base: 0x4000.7000 (p416 datasheet de lm3s9B92.pdf)
+
+; configure the corresponding pin to be an output
+; all GPIO pins are inputs by default
+GPIO_O_DIR  		EQU 	0x00000400  ; GPIO Direction (p417 datasheet de lm3s9B92.pdf)
+
+; The GPIODR2R register is the 2-mA drive control register
+; By default, all GPIO pins have 2-mA drive.
+GPIO_O_DR2R  		EQU 	0x00000500  ; GPIO 2-mA Drive Select (p428 datasheet de lm3s9B92.pdf)
+
+; Digital enable register
+; To use the pin as a digital input or output, the corresponding GPIODEN bit must be set.
+GPIO_O_DEN  		EQU 	0x0000051C  ; GPIO Digital Enable (p437 datasheet de lm3s9B92.pdf)
+
+; Pul_up
+GPIO_I_PUR  		EQU 	0x00000510  ; GPIO Pull-Up (p432 datasheet de lm3s9B92.pdf)
+
+; Broches select
+BROCHE4				EQU		0x10		; led1 broche 4
+BROCHE5				EQU		0x20		; led2 broche 5
+BROCHE4_5			EQU		0x30		; led1 & led2 sur broche 4 et 5
+
+; blinking frequency
+DUREE  				EQU  	0x002FFFFF
+
+
+		; ;; Enable the Port F peripheral clock 		(p291 datasheet de lm3s9B96.pdf)
+ledsInit
+		ldr r6, = SYSCTL_PERIPH_GPIO 			;; RCGC2
+        ldr r0, [r6]                            
+        orr r0, r0, #0x00000020 					;; Enable clock sur GPIO F où sont branchés les leds en utilisant un ou binaire pour pas écraser
+													;; orr = mot-clé montré par chatgpt
+        str r0, [r6]
+
+		; ;;														 									
+		
+		; ;; "There must be a delay of 3 system clocks before any GPIO reg. access  (p413 datasheet de lm3s9B92.pdf)                         
+		nop 	 								;; tres tres important....
+		nop 
+		nop 										;; pas necessaire en simu ou en debbug step by step...
+		
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION LED
+        
+        ldr r0, = BROCHE4_5 	
+        
+		ldr r6, = GPIO_PORTF_BASE + GPIO_O_DIR 	;; 1 Pin du portF en sortie (broche 4 : 00010000)
+        str r0, [r6]
+		
+        
+		ldr r6, = GPIO_PORTF_BASE + GPIO_O_DEN	;; Enable Digital Function
+        str r0, [r6]
+		
+        
+		ldr r6, = GPIO_PORTF_BASE + GPIO_O_DR2R	;; Choix de l'intensité de sortie (2mA)
+        str r0, [r6]
+        
+		ldr r6, = GPIO_PORTF_BASE + (BROCHE4<<2)  ;; @data Register = @base + (mask<<2) ==> LED1
+		ldr r7, = GPIO_PORTF_BASE + (BROCHE5<<2)  ;; @data Register = @base + (mask<<2) ==> LED2
+		ldr r8, = GPIO_PORTF_BASE + (BROCHE4_5<<2)  ;; @data Register = @base + (mask<<2) ==> LED1&2
+		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration LED
+        
+        BX LR                                    
+
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^Fonctions LED
+LedON_1
+	ldr r0,= BROCHE4
+	str r0, [r6]
+	BX LR
+	
+LedOFF_1
+	mov r0, #0x00
+	str r0, [r6]
+	BX LR
+	
+LedON_2
+	ldr r0,= BROCHE5
+	str r0, [r7]
+	BX LR
+	
+LedOFF_2
+	mov r0, #0x00
+	str r0, [r7]
+	BX LR
+	
+LedsON_1_2
+	ldr r0,= BROCHE4_5
+	str r0, [r8]
+	BX LR
+	
+LedsOFF_1_2
+	mov r0, #0x00
+	str r0, [r8]
+	BX LR
+		
+		nop		
+		END
---
+++ Switchs.s
@@ -0,0 +1,68 @@
+AREA    |.text|, CODE, READONLY
+		ALIGN
+
+; This register controls the clock gating logic in normal Run mode
+SYSCTL_PERIPH_GPIO  EQU		0x400FE108	; SYSCTL_RCGC2_R (p291 datasheet de lm3s9b92.pdf)
+
+; The GPIODATA register is the data register
+GPIO_PORTD_BASE		EQU		0x40007000		; GPIO Port D (APB) base: 0x4000.7000 (p416 datasheet de lm3s9B92.pdf)
+
+; configure the corresponding pin to be an output
+; all GPIO pins are inputs by default
+GPIO_O_DIR  		EQU 	0x00000400  ; GPIO Direction (p417 datasheet de lm3s9B92.pdf)
+
+; The GPIODR2R register is the 2-mA drive control register
+; By default, all GPIO pins have 2-mA drive.
+GPIO_O_DR2R  		EQU 	0x00000500  ; GPIO 2-mA Drive Select (p428 datasheet de lm3s9B92.pdf)
+
+; Digital enable register
+; To use the pin as a digital input or output, the corresponding GPIODEN bit must be set.
+GPIO_O_DEN  		EQU 	0x0000051C  ; GPIO Digital Enable (p437 datasheet de lm3s9B92.pdf)
+
+; Pul_up
+GPIO_I_PUR  		EQU 	0x00000510  ; GPIO Pull-Up (p432 datasheet de lm3s9B92.pdf)
+
+; Broches select
+BROCHE6				EQU 	0x40		; switcher 1
+BROCHE7				EQU		0x80		; switcher 2
+BROCHE6_7			EQU		0xc0		; switcher 1 et 2
+	
+switchersInit
+	; ;; Enable the Port D peripheral clock 		(p291 datasheet de lm3s9B96.pdf)									
+		ldr r6, =SYSCTL_PERIPH_GPIO 			;; RCGC2
+		ldr r0, [r6]                            
+        orr r0, r0, #0x08 					    ;; Enable clock sur GPIO D où sont branchés les switchers sans détruire la conf des autres ports des autres
+												;; fichiers (c'est un ou binaire)
+        str r0, [r6]
+		
+		nop 								
+		nop 
+		nop 
+	;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION Switcher 1 et 2
+
+		ldr r7, =GPIO_PORTD_BASE+GPIO_I_PUR	    ;; Pul_up 
+        ldr r0, =BROCHE6_7		
+        str r0, [r7]
+		
+		ldr r7, =GPIO_PORTD_BASE+GPIO_O_DEN	    ;; Enable Digital Function 
+        ldr r0, =BROCHE6_7	
+        str r0, [r7]      
+		
+		ldr r7, =GPIO_PORTD_BASE + (BROCHE6<<2)   ;; @data Register = @base + (mask<<2) ==> Switcher1
+		ldr r8, =GPIO_PORTD_BASE + (BROCHE7<<2)   ;; @data Register = @base + (mask<<2) ==> Switcher2
+		
+	;vvvvvvvvvvvvvvvvvvvvvvvFin configuration Switchers
+		BX LR                                     ;; <--- INDISPENSABLE : Fin de l'init
+
+readSwitch1
+		ldr r0, [r7] ;; Résultat stocké dans le registre de retour r0
+		BX LR
+
+readSwitch2
+		ldr r0, [r8] ;; Résultat stocké dans le registre de retour r0
+		BX LR
+		
+;fin du programme
+		NOP
+		NOP
+		END
