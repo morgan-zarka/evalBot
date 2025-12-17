6264846c912cfb0b69d5aa51a774afef25654556
FARAH Mohamed <mohamed.farah@edu.esiee.fr>
2025-12-17 11:48:26 +0100

    Fixing register bug + Adding Passive blinking + Switcher1 launching

--- Bumpers.s
+++ Bumpers.s
@@ -27,41 +27,36 @@ R11_ADDR_BUMPERS	EQU		11          ; Registre R11 pour l'adresse groupée
 		EXPORT  readBumpers0_1
 			
 bumpersInit
-		
-	ldr r6, = SYSCTL_PERIPH_GPIO 			;; RCGC2
-    ldr r0, [r6]                            ; Lecture de l'état actuel (R-M-W)
-    orr r0, r0, #0x10 						;; Enable clock sur GPIO E 0x10 = ob010000
-	str r0, [r6]
-		
-	; Délai obligatoire (lecture du registre après activation)
-    ldr r0, [r6]                            ; Lecture de RCGC2 pour garantir le délai (Correction)
-	nop
-	nop
-	
-	ldr r5, = GPIO_PORTE_BASE		; R5 contient la base du Port E
-	ldr r0, = BROCHES_BUMPER0_1		; 0x03 (Masque)
-		
-		; Pull-Up
-	ldr r7, = GPIO_PORTE_BASE+GPIO_I_PUR
-	str r0, [r7]
-		
-		; Digital Enable
-	ldr r7, = GPIO_PORTE_BASE+GPIO_O_DEN
-	str r0, [r7]
-		
-		; R9 = Adresse Data Reg. Bumper 0 (PE0)
-	ldr r9, = GPIO_PORTE_BASE + (BROCHE_BUMPER0<<2)
-		; R10 = Adresse Data Reg. Bumper 1 (PE1)
-	ldr r10, = GPIO_PORTE_BASE + (BROCHE_BUMPER1<<2)
     
-    ; R11 = Adresse Data Reg. Bumper 0 et 1 (PE0 | PE1)
+    ldr r6, = SYSCTL_PERIPH_GPIO 
+    ldr r0, [r6]                   
+    orr r0, r0, #0x10 
+    str r0, [r6]
+        
+    nop
+    nop
+    nop
+
+    ldr r1, = GPIO_PORTE_BASE      
+    ldr r0, = BROCHES_BUMPER0_1
+        
+    ; Pull-Up
+    ldr r2, = GPIO_PORTE_BASE+GPIO_I_PUR
+    str r0, [r2]
+        
+    ; Digital Enable
+    ldr r2, = GPIO_PORTE_BASE+GPIO_O_DEN
+    str r0, [r2]
+        
+    ldr r9, = GPIO_PORTE_BASE + (BROCHE_BUMPER0<<2)
+    ldr r10, = GPIO_PORTE_BASE + (BROCHE_BUMPER1<<2)
     ldr r11, = GPIO_PORTE_BASE + (BROCHES_BUMPER0_1<<2)
-	
-	BX LR
+    
+    BX LR
 	
 readBumper0
 	ldr 	r0, [r9]					; Lecture de PE0 (Bumper 0)
-	BX LR								
+	BX 	LR								
 
 readBumper1
 	ldr 	r0, [r10]					; Lecture de PE1 (Bumper 1)
--- Main.s
+++ Main.s
@@ -1,4 +1,8 @@
 		AREA    |.text|, CODE, READONLY
+DUREE_NORMAL   EQU     0x001FFFFF	; Random Value
+DUREE_RAPIDE   EQU		0x000FFFFF
+
+			
 		ENTRY
 		EXPORT	__main
 			
@@ -31,13 +35,49 @@
         IMPORT  MOTEUR_GAUCHE_AVANT
         IMPORT  MOTEUR_GAUCHE_ARRIERE
         IMPORT  MOTEUR_GAUCHE_INVERSE
-			;;; Registres utilisés : switch : R7 et R8 ;;; bumpers : R9,R10,R11 ;;; Moteurs : r6 ;;; Leds : R5
-			;;; Registre DISPONIBLE : R1,R2,R3,R4,R12
+		IMPORT  setRapidMode
+		IMPORT  setNormalMode
+
+			;;; Registres utilisés : switch : R7 et R8 ;;; bumpers : R1,R2,R9,R10,R11 ;;; Moteurs : r6 ;;; Leds : R5
+			;;; Registre DISPONIBLE : ,R3,R4,R12
 __main
 	BL LedsInit
 	BL switchersInit
 	BL bumpersInit
-	BL MOTEUR_INIT
+	BL MOTEUR_INIT	
+loop
+	BL readSwitch1
+	CMP r0, #0x000
+	BEQ startMotors
+	B loop
+	
+startMotors
+	BL MOTEUR_DROIT_ON
+	BL MOTEUR_GAUCHE_ON	
+	BL MOTEUR_DROIT_AVANT	   
+	BL MOTEUR_GAUCHE_AVANT
+	b mainLoop
+	
+mainLoop
+	BL ToggleLeds
+	BL InitDelay
+	
+	B mainLoop
+	
+InitDelay
+	ldr r0, =DUREE_NORMAL
+	b delay
+	BX LR
+	
+delay
+	subs r0, #1
+	CMP r0, #0x000
+	BNE delay
+	
+	BX LR
+	
+	
+	
 	
 	END
 	
